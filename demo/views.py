fd31c5105baa40b50d543b6693daef1730f24b4a
Goran Petrov <boscaiolo.gp@gmail.com>
2020-02-17 20:52:10 +0000

    adding more views

--- demo/views.py
+++ demo/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World !!!")
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You are looking at the results for question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
