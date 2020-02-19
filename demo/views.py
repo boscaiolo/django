aa1c4d204549a758441d2ea2c5311c0453ec107b
Goran Petrov <boscaiolo.gp@gmail.com>
2020-02-19 22:04:54 +0000

    Voting logic and template

--- demo/views.py
+++ demo/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
@@ -14,8 +15,17 @@ def detail(request, question_id):
     return render(request, 'demo/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You are looking at the results for question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'demo/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request,
+            'demo/detail.html', {'question': question, 'error_message': "You did not select a choice."})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('demo:results', args=(question.id,)))
